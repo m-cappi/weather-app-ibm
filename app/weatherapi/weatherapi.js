a27ad9c13c0bd48134ec897fc4db11965abd63e3
Martin Cappi <mcappi91@gmail.com>
2021-11-21 19:07:35 -0300

    removing hourly+daily getter because they are a premium feature

--- app/weatherapi/weatherapi.js
+++ app/weatherapi/weatherapi.js
@@ -13,21 +13,6 @@ class WeatherApi {
     );
   }
 
-  async hourlyWeather(city) {
-    //City name, state code and country code divided by comma, use ISO 3166 country codes. You can specify the parameter not only in English.
-    return await axios.get(
-      `pro.openweathermap.org/data/2.5/forecast/hourly?q=${city}&appid=${this.apiKey}&lang=es&units=metric`,
-    );
-  }
-
-  async dailyWeather(city, days = 7) {
-    //City name, state code and country code divided by comma, use ISO 3166 country codes. You can specify the parameter not only in English.
-    //A number of days, which will be returned in the API response (from 1 to 7) *16 days if paid apiKey
-    return await axios.get(
-      `api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=${days}&appid=${this.apiKey}&lang=es&units=metric`,
-    );
-  }
-
   async getIcon(icon) {
     return await axios.get(`http://openweathermap.org/img/wn/${icon}@2x.png`);
   }
