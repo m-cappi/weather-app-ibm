aa64900e1c24380e24ad59e6242ed07dd75147c8
eugebaquero <85105814+eugebaquero@users.noreply.github.com>
2021-11-27 20:04:41 -0300

    Update DemoStack.js

--- app/navigations/DemoStack.js
+++ app/navigations/DemoStack.js
@@ -3,6 +3,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import DemoScreen from '../screens/Demo/DemoScreen';
 import DemoSubScreen from '../screens/Demo/DemoSubScreen';
+import DemoQuienesScreen from '../screens/Demo/DemoQuienesScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +19,11 @@ const DemoStack = () => {
         name="demo-sub"
         component={DemoSubScreen}
         options={{title: 'Sub-screen demostrativo'}}
+      />
+      <Stack.Screen
+        name="demo-quienes"
+        component={DemoQuienesScreen}
+        options={{title: "Nosotros somos"}}
       />
     </Stack.Navigator>
   );
