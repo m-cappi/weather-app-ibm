5d28055c65a422bf485e1096b6d01e5c6b3b218e
Martin Cappi <mcappi91@gmail.com>
2021-11-27 12:04:09 -0300

    city search

--- app/screens/Weather/Weather.js
+++ app/screens/Weather/Weather.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import SearchBox from '../../components/Weather/SearchBox';
 
 const Weather = () => {
   return (
     <View>
       <Text>Weather screen...</Text>
+      <SearchBox />
     </View>
   );
 };
